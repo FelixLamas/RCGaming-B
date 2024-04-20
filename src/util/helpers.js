6ba3440ff76c96ca7361eadd9f3491622d80d038
jimena <herrerajimena01@gmail.com>
2024-04-19 21:15:45 -0300

    se creo el modelo, controlador y rutas para crear un usuario

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,3 +1,15 @@
+const regexEmail=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const regexPassword=/^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/;
+
+function validateEmail(email){
+    return regexEmail.test(email)
+}
+
+function validatePassword(password){
+    return  regexPassword.test(password)
+}
+
+
 function validateName(name) {
   if (name.length >= 4 && name.length <= 50) {
     return true;
@@ -37,4 +49,6 @@ module.exports = {
   validateStock,
   validateImageUrl,
   validateCharacteristic,
+  validateEmail,
+  validatePassword
 };
