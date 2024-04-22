151708ba26dab7d35b179472562971edebfae554
Felix ELoy Lamas <felamas@gmail.com>
2024-04-22 20:24:51 -0300

    se agregaron atributos al schema de usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,64 +1,76 @@
-const userModel= require("../models/UserModel")
-const bcrypt=require("bcrypt")
-const { validatePassword, validateEmail} = require("../util/helpers")
+const userModel = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const {
+  validatePassword,
+  validateEmail,
+  validaNameUser,
+} = require("../util/helpers");
 
-class UserController{
-    async CreateNewUser(email,password){
-        try {
-            if (!validateEmail(email)) {
-                throw new Error("Formato de email invalido")
-            }
-            if (!validatePassword(password)){
-                throw new Error ("Formato de password incorrecto")
-            }
-            const SALT=parseInt(process.env.BCRYPT_SALT);
-            const hash=await bcrypt.hash(password, SALT);
-            const newUser= new userModel({
-                email:email,
-                password:hash,
-                role:"user"
-            });
-    
-            const savedUser=await newUser.save();
-            return savedUser;
-        } catch (error) {
-            throw error
-        }
+class UserController {
+  async CreateNewUser(name, email, password) {
+    try {
+      if (!validaNameUser(name)) {
+        throw new Error("Nombre invalido");
+      }
+      if (!validateEmail(email)) {
+        throw new Error("Formato de email invalido");
+      }
+      if (!validatePassword(password)) {
+        throw new Error("Formato de password incorrecto");
+      }
+      const SALT = parseInt(process.env.BCRYPT_SALT);
+      const hash = await bcrypt.hash(password, SALT);
+      const newUser = new userModel({
+        name: name,
+        email: email,
+        password: hash,
+        role: "user",
+        isActive: true,
+      });
+
+      const savedUser = await newUser.save();
+      return savedUser;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async CreateNewAdmin(email,password){
-        try {
-            if (!validateEmail(email)) {
-                throw new Error("Formato de email invalido")
-            }
-            if (!validatePassword(password)){
-                throw new Error ("Formato de password incorrecto")
-            }
-            const SALT=parseInt(process.env.BCRYPT_SALT);
-            const hash=await bcrypt.hash(password, SALT);
-            const newUser= new userModel({
-                email:email,
-                password:hash,
-                role:"admin"
-            });
-    
-            const savedUser=await newUser.save();
-            return savedUser;
-        } catch (error) {
-            throw error
-        }
+  async CreateNewAdmin(name, email, password) {
+    try {
+      if (!validaNameUser(name)) {
+        throw new Error("Nombre invalido");
+      }
+      if (!validateEmail(email)) {
+        throw new Error("Formato de email invalido");
+      }
+      if (!validatePassword(password)) {
+        throw new Error("Formato de password incorrecto");
+      }
+      const SALT = parseInt(process.env.BCRYPT_SALT);
+      const hash = await bcrypt.hash(password, SALT);
+      const newUser = new userModel({
+        name: name,
+        email: email,
+        password: hash,
+        role: "admin",
+        isActive: true,
+      });
+
+      const savedUser = await newUser.save();
+      return savedUser;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async DeleteUser(id){
-        try {
-            const deleteUser = await userModel.findByIdAndDelete(id);
-            return deleteUser
-        } catch (error) {
-            throw error
-        }
-
+  async DeleteUser(id) {
+    try {
+      const deleteUser = await userModel.findByIdAndDelete(id);
+      return deleteUser;
+    } catch (error) {
+      throw error;
     }
+  }
+}
 
-    }
-
-module.exports=UserController
+module.exports = UserController;
