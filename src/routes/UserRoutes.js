ace5f99550bb3d1cc8423798cf7ae2ae6ee61c18
Felix ELoy Lamas <felamas@gmail.com>
2024-04-27 21:38:38 -0300

    Se agregaron los middleware de validaciones

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,19 +1,24 @@
 const { response } = require("express");
 const UserController = require("../controllers/UserController");
-
+const Auth = require("../util/Auth");
 const UserRoutes = (base, app) => {
   const userController = new UserController();
-  app.post(`${base}/create-admin`, async (req, res) => {
-    try {
-      const { name, email, password } = req.body;
-      await userController.CreateNewAdmin(name, email, password);
-      return res.status(201).json({ message: "Exito al crear el usuario" });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ message: "Ocurrio un error al intentar crear el usuario" });
+  app.post(
+    `${base}/create-admin`,
+    Auth.isAuth,
+    Auth.isAdmin,
+    async (req, res) => {
+      try {
+        const { name, email, password } = req.body;
+        await userController.CreateNewAdmin(name, email, password);
+        return res.status(201).json({ message: "Exito al crear el usuario" });
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: "Ocurrio un error al intentar crear el usuario" });
+      }
     }
-  });
+  );
 
   app.post(`${base}/create-user`, async (req, res) => {
     try {
@@ -28,7 +33,7 @@ const UserRoutes = (base, app) => {
     }
   });
 
-  app.put(`${base}/modify/:id`, async (req, res) => {
+  app.put(`${base}/modify/:id`, Auth.isAuth, Auth.isAdmin, async (req, res) => {
     try {
       const id = req.params.id;
       const newData = req.body;
@@ -41,32 +46,44 @@ const UserRoutes = (base, app) => {
     }
   });
 
-  app.put(`${base}/activate/:id`, async (req, res) => {
-    try {
-      const id = req.params.id;
-      const isActive = req.body.isActive;
-      await userController.ActivateUser(id, isActive);
-      return res
-        .status(200)
-        .json({ message: "Exito al cambiar el estado del usuario" });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ message: "Error al intentar cambiar el estado del usuario" });
+  app.put(
+    `${base}/activate/:id`,
+    Auth.isAuth,
+    Auth.isAdmin,
+    async (req, res) => {
+      try {
+        const id = req.params.id;
+        const isActive = req.body.isActive;
+        await userController.ActivateUser(id, isActive);
+        return res
+          .status(200)
+          .json({ message: "Exito al cambiar el estado del usuario" });
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: "Error al intentar cambiar el estado del usuario" });
+      }
     }
-  });
+  );
 
-  app.delete(`${base}/delete/:id`, async (req, res) => {
-    try {
-      const id = req.params.id;
-      await userController.DeleteUser(id);
-      return res.status(200).json({ message: "Exito al eliminar el usuario" });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ message: "Error al intentar eliminar el usuario" });
+  app.delete(
+    `${base}/delete/:id`,
+    Auth.isAuth,
+    Auth.isAdmin,
+    async (req, res) => {
+      try {
+        const id = req.params.id;
+        await userController.DeleteUser(id);
+        return res
+          .status(200)
+          .json({ message: "Exito al eliminar el usuario" });
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: "Error al intentar eliminar el usuario" });
+      }
     }
-  });
+  );
 
   app.post(`${base}/login`, async (req, res, next) => {
     try {
