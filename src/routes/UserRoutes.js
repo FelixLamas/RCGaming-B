fdceb29b0ab3434858b3e35928d65385ea14629a
Felix ELoy Lamas <felamas@gmail.com>
2024-05-09 19:33:18 -0300

    se corrigio error al obtener los usuario

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -111,21 +111,15 @@ const UserRoutes = (base, app) => {
         .json({ message: "Error al modificar la contraseña" });
     }
   });
-  
+
   app.get(`${base}/getUsers`, Auth.isAuth, Auth.isAdmin, async (req, res) => {
     try {
       const users = await userController.getAllUsers();
-      const usersWithoutPassword = users.map(user => {
-        const { password, ...userWithoutPassword } = user;
-        return userWithoutPassword;
-      });
-      return res.status(200).json(usersWithoutPassword);
+      return res.status(200).json(users);
     } catch (error) {
       return res.status(500).json({ message: "Error al obtener los usuarios" });
     }
   });
-
-
 };
 
 module.exports = UserRoutes;
